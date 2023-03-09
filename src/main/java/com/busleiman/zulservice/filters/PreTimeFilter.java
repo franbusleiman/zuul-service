efd21fc5ca29c2cbcce38ee43ddbcf448ae0d26f
franbusleiman <Sabueso4917>
2023-03-09 10:48:30 -0300

    Resource service changes in order to accept pets-service

--- src/main/java/com/busleiman/zulservice/filters/PreTimeFilter.java
+++ src/main/java/com/busleiman/zulservice/filters/PreTimeFilter.java
@@ -5,6 +5,8 @@ import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,12 +36,23 @@ public class PreTimeFilter extends ZuulFilter {
     public Object run() throws ZuulException {
 
         logger.info("Crossing the edge");
+        String username;
 
         RequestContext rx = RequestContext.getCurrentContext();
         HttpServletRequest httpServletRequest = rx.getRequest();
 
         Long currentTime = System.currentTimeMillis();
         httpServletRequest.setAttribute("currentTime", currentTime);
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        logger.info("USERNAME: " + username);
 
         return null;
     }
--- src/main/java/com/busleiman/zulservice/oauth/ResourceServiceConfig.java
+++ src/main/java/com/busleiman/zulservice/oauth/ResourceServiceConfig.java
@@ -16,19 +16,22 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServiceConfig extends ResourceServerConfigurerAdapter {
 
 
-
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-       resources.tokenStore(tokenStore());
+        resources.tokenStore(tokenStore());
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/api/security/oauth/token").permitAll()
-        .antMatchers(HttpMethod.GET, "api/products/products", "api/items/items", "/api/users/users").permitAll()
-        .antMatchers(HttpMethod.GET, "/api/products/products/{id}", "/api/items/items/{id}/quantity/{quantity}}", "/api/users/users/{id}").hasAnyRole("ADMIN")
-        .antMatchers(HttpMethod.POST, "api/products/products/", "api/items/items/", "/api/users/users/").hasAnyRole("ADMIN")
-        .anyRequest().authenticated();
+                .antMatchers( "/api/pets/h2-console/**", "/api/users/h2-console/**",  "/api/pets/actuator/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/users/users").permitAll()
+                .antMatchers(HttpMethod.GET, "api/products/products", "api/items/items", "/api/users/users").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/products/products/{id}", "/api/items/items/{id}/quantity/{quantity}}", "/api/users/users/{id}").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "api/products/products/", "api/items/items/").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .headers().frameOptions().disable();
     }
 
 
@@ -39,7 +42,7 @@ public class ResourceServiceConfig extends ResourceServerConfigurerAdapter {
 
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter jwtAccessTokenConverter =  new JwtAccessTokenConverter();
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
 
         jwtAccessTokenConverter.setSigningKey("codigo_secreto");
 
